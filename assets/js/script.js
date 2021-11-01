b0b3333844e0ae7df19d946e284e22f4f9c42e9e
bvsswave <tannershahan@gmail.com>
2021-10-31 21:22:19 -0600

    adding animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,61 @@
+// animation - t
+
+$(document).ready(function () {
+	function detect_active() {
+	  // get active
+	  var get_active = $("#dp-slider .dp_item:first-child").data("class");
+	  $("#dp-dots li").removeClass("active");
+	  $("#dp-dots li[data-class=" + get_active + "]").addClass("active");
+	}
+	$("#dp-next").click(function () {
+	  var total = $(".dp_item").length;
+	  $("#dp-slider .dp_item:first-child").hide().appendTo("#dp-slider").fadeIn();
+	  $.each($(".dp_item"), function (index, dp_item) {
+		$(dp_item).attr("data-position", index + 1);
+	  });
+	  detect_active();
+	});
+  
+	$("#dp-prev").click(function () {
+	  var total = $(".dp_item").length;
+	  $("#dp-slider .dp_item:last-child").hide().prependTo("#dp-slider").fadeIn();
+	  $.each($(".dp_item"), function (index, dp_item) {
+		$(dp_item).attr("data-position", index + 1);
+	  });
+  
+	  detect_active();
+	});
+  
+	$("#dp-dots li").click(function () {
+	  $("#dp-dots li").removeClass("active");
+	  $(this).addClass("active");
+	  var get_slide = $(this).attr("data-class");
+	  console.log(get_slide);
+	  $("#dp-slider .dp_item[data-class=" + get_slide + "]")
+		.hide()
+		.prependTo("#dp-slider")
+		.fadeIn();
+	  $.each($(".dp_item"), function (index, dp_item) {
+		$(dp_item).attr("data-position", index + 1);
+	  });
+	});
+  
+	$("body").on("click", "#dp-slider .dp_item:not(:first-child)", function () {
+	  var get_slide = $(this).attr("data-class");
+	  console.log(get_slide);
+	  $("#dp-slider .dp_item[data-class=" + get_slide + "]")
+		.hide()
+		.prependTo("#dp-slider")
+		.fadeIn();
+	  $.each($(".dp_item"), function (index, dp_item) {
+		$(dp_item).attr("data-position", index + 1);
+	  });
+  
+	  detect_active();
+	});
+  });
+  
+
 var steamAPI = function(userID, playerSummary, playerStats, ) {
 
 	var steamKey = "4F3B76E047AA7DDB2C31FE9E7A61DD0E"
@@ -10,6 +68,11 @@ var steamAPI = function(userID, playerSummary, playerStats, ) {
 
 }
 
+// end of animation -t
+
 console.log(userStats);
 
 steamAPI();
+
+
+
