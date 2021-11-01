9ef8de811ab034a30adbdb2446e68a8de1f0f1a0
Nick Rizzo <nickrizzo77277@gmail.com>
2021-10-31 20:57:39 -0500

    got api to pull and display data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,28 @@
-var steamAPI = function(playerSummary) {
 
-	// var steamKey = "4F3B76E047AA7DDB2C31FE9E7A61DD0E"
-	// var userID = '76561198024405122'
-	// var userStats = 'http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=440&key=${steamKey}&steamid=${userID}'
 
-	var playerSummary =  'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=4F3B76E047AA7DDB2C31FE9E7A61DD0E&steamids=76561198024405122'
 
-	// var userAchievements = 'http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=440&key=${steamKey}&steamid=${userID}'
+var steamAPI = playerSummary => {
 
-	fetch(playerSummary)
+	var steamKey = "4F3B76E047AA7DDB2C31FE9E7A61DD0E"
+	var userID = "76561198024405122"
 
-	.then(function (response) {
-		if (response.ok) {
-			console.log("Resonse OK", response);
-			response.json()
 
-			console.log(playerSummary);
 
+	var playerSummary =  
 	
-		}
-	})
+	"https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key='steamKey'&steamids=76561198024405122"
 
 	console.log("Inside Stream api ", playerSummary); 
 
 	fetch(playerSummary, {
-		// mode: 'no-cors',
 		method: 'GET', 
 		credentials: 'same-origin',
         headers: {
-            'Content-Type': 'application/json',
-			'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/hal+json',
+			'origin': 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=4F3B76E047AA7DDB2C31FE9E7A61DD0E&steamids=76561198024405122'
         },
     }).then(response => {
-        console.log(response.body)
+        // console.log(response)
         return response.json()
     }).then(data => {
         console.log(data);
@@ -40,8 +30,7 @@ var steamAPI = function(playerSummary) {
     }).catch(error => {
         console.error(error);
     });
-};
+	};
 
 
 steamAPI();
-
