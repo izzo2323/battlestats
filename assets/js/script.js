d245a8e126e958a07f8a395d7396a651aa09cf22
Nick Rizzo <nickrizzo77277@gmail.com>
2021-11-01 18:38:44 -0500

    updated API functionality and game list sort criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,93 @@
-var steamAPI = playerSummary => {
+var steamAPI = (playerSummary) => {
+  var steamKey = "4F3B76E047AA7DDB2C31FE9E7A61DD0E";
+  var userID = document.getElementById("username").value;
 
+  console.log("UserID is:", userID);
 
+  var playerSummary = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=4F3B76E047AA7DDB2C31FE9E7A61DD0E&steamids=${userID}`;
 
-	var steamKey = "4F3B76E047AA7DDB2C31FE9E7A61DD0E"
-	var userID = document.getElementById('#input').value;
+  console.log("Inside Stream api ", playerSummary);
 
-console.log(userID)
+  fetch(playerSummary, {
+    method: "GET",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/hal+json",
+      origin: `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=4F3B76E047AA7DDB2C31FE9E7A61DD0E&steamids=${userID}`,
+    },
+  })
+    .then((response) => {
+      // console.log(response)
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Player SUmmmary Data", data);
 
-	var playerSummary = "https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=4F3B76E047AA7DDB2C31FE9E7A61DD0E&steamids=$(userID)"
+      ///Steam API - Get Player Games
+      var getPlayerGamesList = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?&include_appinfo=true&include_played_free_games=true&key=${steamKey}&steamid=${userID}&format=json`;
 
-	console.log("Inside Stream api ", playerSummary); 
-
-	fetch(playerSummary, {
-		method: 'GET', 
-		credentials: 'same-origin',
+      fetch(getPlayerGamesList, {
+        method: "GET",
+        credentials: "same-origin",
         headers: {
-            'Content-Type': 'application/hal+json',
-			'origin': 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=4F3B76E047AA7DDB2C31FE9E7A61DD0E&steamids=userID'
+          "Content-Type": "application/hal+json",
+          origin: `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?&include_appinfo=true&include_played_free_games=true&key=${steamKey}&steamid=${userID}&format=json`,
         },
-    }).then(response => {
-        // console.log(response)
-        return response.json()
-    }).then(data => {
-        console.log(data);
+      })
+        .then((achievemntResponse) => {
+          // console.log(response)
+          return achievemntResponse.json();
+        })
+        .then((playerGamesListData) => {
+          console.log("get player Game list", playerGamesListData);
 
-    }).catch(error => {
-        console.error(error);
+          //   console.log(
+          //     "Sorting data in Array ",
+          //     playerGamesListData.response.games
+          //   );
+          //76561198024405122
+          //Sorting data in an Array to show top played games
+          var sorted = playerGamesListData.response.games.sort(function (
+            productA,
+            productB
+          ) {
+            if (productA.playtime_forever > productB.playtime_forever) {
+              return -1;
+            }
+            if (productB.playtime_forever > productA.playtime_forever) {
+              return 1;
+            }
+            return 0;
+          });
+          console.log("Sorted data is ", sorted);
+
+          //top 5
+
+          for (let i = 0; i < playerGamesListData.length; i++) {
+            if (playerGamesListData[4]) {
+              playerGamesListData[4]++;
+            }
+            return playerGamesListData[4];
+          }
+          console.log(playerGamesListData[4]);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    })
+    .catch((error) => {
+      console.error(error);
     });
-	};
+};
 
 steamAPI();
 
+var searchBtn = document.getElementsByClassName("btn");
 
+console.log("btn", searchBtn[0]);
+
+searchBtn[0].addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log(event);
+  steamAPI();
+});
