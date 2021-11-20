76797697215b3097e2a59f2e7430657c8b4672e1
Nick Rizzo <nickrizzo77277@gmail.com>
2021-11-20 12:59:32 -0600

    created steam.js file

---
+++ assets/js/steam.js
@@ -0,0 +1,128 @@
+var steamAPI = (playerSummary) => {
+    var steamKey = "4F3B76E047AA7DDB2C31FE9E7A61DD0E";
+    // var userID = document.getElementById("username").value;
+    var userID = "76561198024405122"; //temp changes
+    // console.log("UserID is:", userID);
+    var playerSummary = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=4F3B76E047AA7DDB2C31FE9E7A61DD0E&steamids=${userID}`;
+    // console.log("Inside Steam api ", playerSummary);
+    fetch(playerSummary, {
+      method: "GET",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/hal+json",
+        origin: `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=4F3B76E047AA7DDB2C31FE9E7A61DD0E&steamids=${userID}`,
+      },
+    })
+      .then((response) => {
+        // console.log(response)
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Player SUmmmary Data", data);
+
+
+
+        var personastateEl = data.response.players[0].personastate (personastateEl => {
+
+        if (personastateEl = 0){
+            return ("Offline")
+        } else if (personastateEl = 1) {
+            return("Online")
+        } else if (personastateEl = 2 ) {
+            return("Busy")
+        } else if (personastateEl = 3) {
+            return("Away")
+        } else if (personastateEl = 4) {
+            return("Snooze")
+        } else if (personastateEl = 5) {
+            return("Looking to Trade")
+        } else if (personastateEl = 6) {
+            return("Looking to Play")
+        }});
+        
+        
+        console.log("PersonaState is ", personastateEl);
+
+
+        var personaNameEl = data.response.players[0].personaname;
+        var imgEl = data.response.players[0].avatarfull;
+  
+       
+        console.log("Avatar is ", imgEl);
+        console.log("Persona Name is ", personaNameEl);
+        // var imgEL = document.getElementById("personaAvatar");
+        // imgEL.setAttribute("src", data.response.players[0].avatar);
+  
+        //Steam API - Get Player Games
+        var getPlayerGamesList = `https://cors-anywhere.herokuapp.com/https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?&include_appinfo=true&include_played_free_games=true&key=${steamKey}&steamid=${userID}&format=json`;
+        fetch(getPlayerGamesList, {
+          method: "GET",
+          credentials: "same-origin",
+          headers: {
+            "Content-Type": "application/hal+json",
+            origin: `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?&include_appinfo=true&include_played_free_games=true&key=${steamKey}&steamid=${userID}&format=json`,
+          },
+        })
+          .then((achievemntResponse) => {
+            // console.log(response)
+            return achievemntResponse.json();
+          })
+          .then((playerGamesListData) => {
+            console.log("get player Game list", playerGamesListData);
+            //   console.log(
+            //     "Sorting data in Array ",
+            //     playerGamesListData.response.games
+            //   );
+            //76561198024405122
+            //Sorting data in an Array to show top played games
+            var sorted = playerGamesListData.response.games.sort(function (
+              productA,
+              productB
+            ) {
+              if (productA.playtime_forever > productB.playtime_forever) {
+                return -1;
+              }
+              if (productB.playtime_forever > productA.playtime_forever) {
+                return 1;
+              }
+              return 0;
+            });
+            console.log("Sorted data is ", sorted);
+            //top 5
+            for (let i = 0; i < playerGamesListData.length; i++) {
+              if (playerGamesListData[4]) {
+                playerGamesListData[4]++;
+              }
+              return playerGamesListData[4];
+            }
+            console.log(playerGamesListData[4]);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  
+  steamAPI();
+  
+//   var searchBtn = document.getElementsByClassName("btn");
+//   console.log("btn", searchBtn[0]);
+//   searchBtn[0].addEventListener("click", (event) => {
+//     event.preventDefault();
+//     console.log(event);
+//     steamAPI();
+//   });
+//   white_check_mark;
+//   eyes;
+//   raised_hands;
+  
+//   console.log("btn", searchBtn[0]);
+  
+//   searchBtn[0].addEventListener("click", (event) => {
+//     event.preventDefault();
+//     console.log(event);
+//     steamAPI();
+// });
