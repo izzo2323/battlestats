db132d3dc29e864ddca4b558520c868023dfecee
bvsswave <tannershahan@gmail.com>
2021-11-23 13:15:41 -0700

    pushing code

---
+++ assets/js/login.js
@@ -0,0 +1,12 @@
+var enter;
+var username;
+
+var Password
+// function username() {
+//     if ( enter <= 5)
+    
+
+//     // if (!enter) {
+//     //     alert("Please enter your username and password");
+//     // }
+// }
